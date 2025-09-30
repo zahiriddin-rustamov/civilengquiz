20c498d42dec6def059f9f6ab8cabcaf2e898347
Zahir Sher <zakisher@gmail.com>
2025-09-30 16:50:40 +0400

    refactor(next.config): add typescript configuration to ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript: {
+    // ⚠️ Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
